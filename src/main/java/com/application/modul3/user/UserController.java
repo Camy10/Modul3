dca585275ed214cd1c8bdd783e8df7fd17825b04
Camy10 <71508413+Camy10@users.noreply.github.com>
2022-02-24 21:33:48 +0200

    UserDTO, UserCreateDTO

--- src/main/java/com/application/modul3/user/UserController.java
+++ src/main/java/com/application/modul3/user/UserController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.application.modul3.user.dto.UserCreateDTO;
+import com.application.modul3.user.dto.UserDTO;
+import com.application.modul3.user.mapper.UserMapper;
+
 @RestController // face ca acest controller sa fie primul nivel din schema noastra si sa stie ca
 				// frontend-ul va comunica cu acest prim nivel
 @RequestMapping("/users") // indica ca toate APIS-urile din controller incept cu 'users'
@@ -18,21 +22,24 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private UserMapper userMapper;
 
 	// build create user Rest API
 	@PostMapping
-	public User createUser(@RequestBody User user) {
-		return userService.createUser(user);
+	public UserDTO createUser(@RequestBody UserCreateDTO userCreateDTO) {
+		User user = userService.createUser(userMapper.userCreateDTO2User(userCreateDTO));
+		return userMapper.user2UserDTO(user);
 	}
 
 	@GetMapping("/list")
-	public List<User> getAllUsers() {
-		return userService.getAllUser();
+	public List<UserDTO> getAllUsers() {
+		return userMapper.userList2userDTOList(userService.getAllUser());
 	}
 
 	@GetMapping("/{id}")
-	public User getUsersById(@PathVariable Integer id) {
-		return userService.getUserById(id);
+	public UserDTO getUsersById(@PathVariable Integer id) {
+		return userMapper.user2UserDTO(userService.getUserById(id));
 	}
 
 	@DeleteMapping("/{id}")
@@ -41,7 +48,8 @@ public class UserController {
 	}
 
 	@PutMapping("/{id}")
-	public User updateUser(@RequestBody User user, @PathVariable Integer id) {
-		return userService.updateUser(user, id);
+	public UserDTO updateUser(@RequestBody UserDTO userDTO, @PathVariable Integer id) {
+		User user = userService.updateUser(userMapper.userDTO2User(userDTO), id);
+		return userMapper.user2UserDTO(user);
 	}
 }
